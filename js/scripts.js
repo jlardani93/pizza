0b8cd66b880b6438184c5f6ababe2823f3ba794b
Justin Lardani <jlardani93@gmail.com>
2018-01-26 13:36:58 -0800

    adds javascript up through selecting ingredients.

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ var Order = {
 };
 
 var Kitchen = {
+  pizza: undefined,
   bases: [],
   toppings: [],
   addBase: function(base) {
@@ -31,6 +32,9 @@ var Kitchen = {
   },
   addTopping: function(topping) {
     this.toppings.push(topping);
+  },
+  getTopping: function(index) {
+    return this.toppings[index];
   }
 };
 
@@ -49,18 +53,47 @@ function Pizza() {
   this.totalPrice = 0;
   this.toppings = [];
   this.base = undefined;
+  this.sizeCost = 0;
+};
+
+Pizza.prototype.setBase = function(base) {
+  this.base = base;
 };
 
 Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping);
 };
 
-Pizza.prototype.removeTopping = function() {},
+Pizza.prototype.removeTopping = function(toppingToRemove) {
+  var index = 0;
+  this.toppings.forEach(function(topping) {
+    if (toppingToRemove === topping) {
+      Kitchen.pizza.toppings.splice(index, index+1)
+    } else {
+      index++;
+    };
+  });
+};
+
+Pizza.prototype.hasTopping = function(possibleTopping) {
+  var hasTopping = false;
+  this.toppings.forEach(function(topping) {
+    if (possibleTopping === topping) {
+      hasTopping = true;
+      console.log(hasTopping);
+    };
+  });
+  return hasTopping;
+}
+
+Pizza.prototype.setSizeCost = function(cost) {
+  this.sizeCost = cost;
+};
 
 Pizza.prototype.setTotalPrice = function() {
-  var totalPrice = 0;
+  var totalPrice = this.base.price + this.sizeCost;
   this.toppings.forEach(function(topping) {
-    totalPrice += this.price;
+    totalPrice += topping.price;
   });
   this.totalPrice = totalPrice;
 };
@@ -89,5 +122,84 @@ var stockKitchen = function() {
 };
 
 $(document).ready(function() {
+
+  var updateOrder = function() {
+    Order.setTotalPrice();
+    Kitchen.pizza.setTotalPrice();
+    var orderTotal = Order.totalPrice + Kitchen.pizza.totalPrice;
+    $("#totalCost").text("Total Cost: $" + orderTotal);
+    $("#pizzasOrdered").text("Pizzas Ordered: " + Order.pizzas.length);
+  };
+
+  stockKitchen();
+
+  $("#placeAnOrder").click(function() {
+    $(this).parent().parent().toggleClass("hidden");
+    $("#formRow").toggleClass("hidden");
+  });
+
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    Customer.setName($("#formName").val());
+    Customer.setAddress($("#formAddress").val());
+    $(this).parent().parent().toggleClass("hidden");
+    $("#pizzaBuilderContainerRow").toggleClass("hidden");
+    Kitchen.pizza = new Pizza();
+  });
+
+  $("#cheeseButton").click(function() {
+    Kitchen.pizza.setBase(Kitchen.bases[0])
+    $(this).parent().parent().toggleClass("hidden");
+    $("#pizzaSizeRow").toggleClass("hidden");
+  });
+
+  $("#noCheeseButton").click(function() {
+    Kitchen.pizza.setBase(Kitchen.bases[1])
+    $(this).parent().parent().toggleClass("hidden");
+    $("#pizzaSizeRow").toggleClass("hidden");
+  });
+
+  $("#smallPizzaButton").click(function() {
+    Kitchen.pizza.setSizecost(0);
+    $(this).parent().parent().toggleClass("hidden");
+    $("#buildPizzaRow").toggleClass("hidden");
+    updateOrder();
+  });
+
+  $("#mediumPizzaButton").click(function() {
+    Kitchen.pizza.setSizeCost(2);
+    $(this).parent().parent().toggleClass("hidden");
+    $("#buildPizzaRow").toggleClass("hidden");
+    updateOrder();
+  });
+
+  $("#largePizzaButton").click(function() {
+    Kitchen.pizza.setSizeCost(4);
+    $(this).parent().parent().toggleClass("hidden");
+    $("#buildPizzaRow").toggleClass("hidden");
+    updateOrder();
+  });
+
+  $(".topping").click(function() {
+    var index = $(this).val();
+    console.log($(this).val());
+    var topping = Kitchen.getTopping(index);
+    if (Kitchen.pizza.hasTopping(topping) === false) {
+      Kitchen.pizza.addTopping(topping);
+      updateOrder();
+      console.log("added topping");
+    } else {
+      Kitchen.pizza.removeTopping(topping);
+      updateOrder();
+      console.log("removed topping");
+    };
+    $(this).toggleClass("selected");
+  });
+
+
+
+
+
+
 
 });
